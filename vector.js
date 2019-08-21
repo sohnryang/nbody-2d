f0aadd1868968e466758170012fd23b6fb175d80
Ryang Sohn <loop.infinitely@gmail.com>
2019-08-21 22:55:40 +0900

    Add a class for 2D vector

---
+++ vector.js
@@ -0,0 +1,20 @@
+export class Vector2D {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  add(vec2d) {
+    const result = new Vector2D(this.x + vec2d.x, this.y + vec2d.y);
+    return result;
+  }
+
+  sub(vec2d) {
+    const result = new Vector2D(this.x - vec2d.x, this.y - vec2d.y);
+    return result;
+  }
+
+  l2norm() {
+    return Math.hypot(this.x, this.y);
+  }
+}
