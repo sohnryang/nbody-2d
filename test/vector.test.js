8580b755599a8e1441ba31bddf014fad5cd68496
Ryang Sohn <loop.infinitely@gmail.com>
2019-08-22 01:31:18 +0900

    Add more test to vector

--- test/vector.test.js
+++ test/vector.test.js
@@ -4,3 +4,24 @@ test('Vector2D constructs with X and Y coordinates', () => {
   const testVec = new Vector2D(1, 2);
   expect(testVec).toEqual({x: 1, y: 2});
 });
+
+test('Two Vector2D adds properly', () => {
+  const testVec1 = new Vector2D(1, 2);
+  const testVec2 = new Vector2D(3, 4);
+  const testVec3 = new Vector2D(4, 6);
+  expect(testVec3).toEqual(testVec1.add(testVec2));
+});
+
+test('Two Vector2D substracts properly', () => {
+  const testVec1 = new Vector2D(1, 2);
+  const testVec2 = new Vector2D(3, 4);
+  const testVec3 = new Vector2D(-2, -2);
+  expect(testVec3).toEqual(testVec1.sub(testVec2));
+});
+
+test('L2 norm in Vector2D is calculated properly', () => {
+  const testVec1 = new Vector2D(3, 4);
+  expect(testVec1.l2norm()).toEqual(5);
+  const testVec2 = new Vector2D(1, 1);
+  expect(testVec2.l2norm()).toEqual(Math.sqrt(2));
+});
