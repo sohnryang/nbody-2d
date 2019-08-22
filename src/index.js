3b27beb5f62a9b24174603b4069f002d76783d36
Ryang Sohn <loop.infinitely@gmail.com>
2019-08-22 13:58:37 +0900

    Make canvas occupy the whole window

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log('Hello, world!')
+const canvas = document.getElementById('simulation');
+const ctx = canvas.getContext('2d');
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
