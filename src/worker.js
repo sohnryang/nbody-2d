d42108abd1c518a93f519662715bcc634347daee
Ryang Sohn <loop.infinitely@gmail.com>
2019-08-23 02:01:10 +0900

    Add body initialization to separate function

--- src/worker.js
+++ src/worker.js
@@ -1,12 +1,19 @@
 import RigidBody from './rigid-body';
 import Vector2D from './vector';
 
-onmessage = function() {
+let bodies = [];
+
+function initializeBodies() {
   const pos1 = new Vector2D(0, 0);
   const vel1 = new Vector2D(0, 0);
   const body1 = new RigidBody(50, 1, pos1, vel1);
   const pos2 = new Vector2D(100, 0);
   const vel2 = new Vector2D(0, 0);
   const body2 = new RigidBody(40, 1, pos2, vel2);
-  postMessage([body1, body2]);
+  bodies = [body1, body2];
+}
+initializeBodies();
+
+onmessage = function() {
+  postMessage(bodies);
 };
