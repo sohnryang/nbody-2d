ea4d41b95d3ad0cbaf27b1f233207d590bd03c66
Ryang Sohn <loop.infinitely@gmail.com>
2019-08-23 01:55:27 +0900

    Add an object to test renderer
    Since I can't automate UI tests, I have no choice but this...

--- src/worker.js
+++ src/worker.js
@@ -5,5 +5,8 @@ onmessage = function() {
   const pos1 = new Vector2D(0, 0);
   const vel1 = new Vector2D(0, 0);
   const body1 = new RigidBody(50, 1, pos1, vel1);
-  postMessage([body1]);
+  const pos2 = new Vector2D(100, 0);
+  const vel2 = new Vector2D(0, 0);
+  const body2 = new RigidBody(40, 1, pos2, vel2);
+  postMessage([body1, body2]);
 };
