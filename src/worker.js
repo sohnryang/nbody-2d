b7764d44a23d6e4c7fd293636defe0dc9d5d3263
Ryang Sohn <loop.infinitely@gmail.com>
2019-08-23 15:57:26 +0900

    Change bodies' position for testing

--- src/worker.js
+++ src/worker.js
@@ -4,10 +4,10 @@ import Vector2D from './vector';
 let bodies = [];
 
 function initializeBodies() {
-  const pos1 = new Vector2D(0, 0);
+  const pos1 = new Vector2D(-100, -50);
   const vel1 = new Vector2D(0, 0);
   const body1 = new RigidBody(50, 1, pos1, vel1);
-  const pos2 = new Vector2D(100, 0);
+  const pos2 = new Vector2D(200, 50);
   const vel2 = new Vector2D(0, 0);
   const body2 = new RigidBody(40, 1, pos2, vel2);
   bodies = [body1, body2];
